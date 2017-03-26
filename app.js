b9edc6691e841ee57b63edfb0bf06148ec80bad0
LokeshS571 <lokeshv542@gmail.com>
2017-03-26 14:20:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var r=5; // radius of the circle
+var a=3.142*r*r; // Area of the circle is calculated
+
+
+// to display the area in the console
+console.log("The Area of the circle for radius ",r,"is ",a); 
+
